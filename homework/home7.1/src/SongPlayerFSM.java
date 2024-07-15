b4ab1335396edd06721ee1bb0890127f5ae4fa78
Katherine Shashkina <kshashkina@kse.org.ua>
2024-07-15 10:34:26 +0300

    made improvements in code readability

--- homework/home7.1/src/SongPlayerFSM.java
+++ homework/home7.1/src/SongPlayerFSM.java
@@ -1,3 +1,5 @@
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SongPlayerFSM {
@@ -19,75 +21,48 @@ public class SongPlayerFSM {
         Scanner scanner = new Scanner(System.in);
         Song currentSong = Song.Intro;
 
+        Map<Song, Map<Command, Song>> transitions = createTransitions();
+
         while (currentSong != Song.End) {
             System.out.println("Currently playing: " + currentSong);
             System.out.print("Enter command (sad, fun, silly, dangerous): ");
             String userInput = scanner.nextLine().trim().toLowerCase();
 
             Command command = getCommand(userInput);
-
-            switch (currentSong) {
-                case Intro:
-                    switch (command) {
-                        case Sad:
-                            currentSong = Song.End;
-                            break;
-                        case Fun:
-                            currentSong = Song.Starman;
-                            break;
-                        case Dangerous:
-                            currentSong = Song.LetItBe;
-                            break;
-                        default:
-                            break; // Replay Intro for other commands
-                    }
-                    break;
-                case Starman:
-                    switch (command) {
-                        case Silly:
-                            currentSong = Song.Intro;
-                            break;
-                        case Dangerous:
-                            currentSong = Song.ShowMustGoOn;
-                            break;
-                        case Fun:
-                            currentSong = Song.End;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case ShowMustGoOn:
-                    switch (command) {
-                        case Sad:
-                            currentSong = Song.LetItBe;
-                            break;
-                        case Fun:
-                            currentSong = Song.Starman;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case LetItBe:
-                    switch (command) {
-                        case Dangerous:
-                            currentSong = Song.Intro;
-                            break;
-                        case Silly:
-                            currentSong = Song.ShowMustGoOn;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            }
+            currentSong = getNextSong(currentSong, command, transitions);
         }
 
         System.out.println("Terminating program. Playing: " + currentSong);
         scanner.close();
+    }
+
+    private static Map<Song, Map<Command, Song>> createTransitions() {
+        Map<Song, Map<Command, Song>> transitions = new EnumMap<>(Song.class);
+
+        transitions.put(Song.Intro, Map.of(
+                Command.Sad, Song.End,
+                Command.Fun, Song.Starman,
+                Command.Dangerous, Song.LetItBe
+        ));
+        transitions.put(Song.Starman, Map.of(
+                Command.Silly, Song.Intro,
+                Command.Dangerous, Song.ShowMustGoOn,
+                Command.Fun, Song.End
+        ));
+        transitions.put(Song.ShowMustGoOn, Map.of(
+                Command.Sad, Song.LetItBe,
+                Command.Fun, Song.Starman
+        ));
+        transitions.put(Song.LetItBe, Map.of(
+                Command.Dangerous, Song.Intro,
+                Command.Silly, Song.ShowMustGoOn
+        ));
+
+        return transitions;
+    }
+
+    private static Song getNextSong(Song currentSong, Command command, Map<Song, Map<Command, Song>> transitions) {
+        return transitions.getOrDefault(currentSong, Map.of()).getOrDefault(command, currentSong);
     }
 
     // Helper method to convert user input string to Command enum
